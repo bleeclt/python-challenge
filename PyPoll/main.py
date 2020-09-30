7c237d984d5e35103d1daec7ab7e083e32b5b001
bleeclt <70173407+bleeclt@users.noreply.github.com>
2020-09-30 16:27:25 -0400

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,6 @@
 import os
 import csv
-import collections
-from collections import Counter
+
 
 election_csv = os.path.join("Resources", "election_data.csv")
 
@@ -15,6 +14,12 @@ with open(election_csv) as csvfile:
     # variable to hold candidates list
     candidates = []
 
+    # variables to hold vote count per candidate
+    Khan_count = 0
+    Correy_count = 0
+    Li_count = 0
+    OTooley_count = 0
+
     # loop to capture all votes 
     for row in csvreader:
         votes.append(str(row[2]))
@@ -23,34 +28,63 @@ print("Election Results")
 print("--------------------------")
 print(f'Total Votes: {len(votes)}')
 print("--------------------------")
-    
-# dictionary to hold the count of each vote
-# breakdown = Counter(votes)
-# get the key and values of each dictionary item
-#for key, value in breakdown.items():
-        
-    #print(f'{key}:  ({value})')
 
-# variables to hold the number of votes for each candidate
-Khan_count = votes.count("Khan")
-Correy_count = votes.count("Correy")
-Li_count = votes.count("Li")
-OTooley_count = votes.count("O'Tooley")
+# loop to build candidates list
+for x in votes:
+    if x not in candidates:
+        candidates.append(x)
+
+# print(len(candidates))
+# print(candidates) 
+ 
+# loop to count votes for each candidate   
+for i in range(0, len(votes)):
+
+    if votes[i] == candidates[0]:
+        Khan_count += 1
+# print(Khan_count)
+
+    elif votes[i] == candidates[1]:
+        Correy_count += 1
+
+    elif votes[i] == candidates[2]:
+        Li_count += 1
+
+    else:
+        OTooley_count +=1
+
+## Alternate method I used intially but less clean and required importing counter
+    #for i in range(1, len(candidates))
+    #   print(candidates)
+        
+    # dictionary to hold the count of each vote
+    # breakdown = Counter(votes)
+    # get the key and values of each dictionary item
+    #for key, value in breakdown.items():
+            
+        #print(f'{key}:  ({value})')
+
+    # variables to hold the number of votes for each candidate
+    #Khan_count = votes.count("Khan")
+    #Correy_count = votes.count("Correy")
+    #Li_count = votes.count("Li")
+    #OTooley_count = votes.count("O'Tooley")
 
 # determine highest vote count
 most_votes = max(Khan_count, Correy_count, Li_count, OTooley_count)
+
 # string to hold winner
 Winner = ""
 
 # determine winner 
 if most_votes == Khan_count:
-    Winner = "Khan"
+    Winner = candidates[0]
 elif most_votes == Correy_count:
-    Winner = "Correy"
+    Winner = candidates[1]
 elif most_votes == Li_count:
-    Winner = "Li"
+    Winner = candidates[2]
 else:
-    Winner = "O'Tooley"
+    Winner = candidates[3]
 
 # variables to calculate the percentage of total votes
 Khan_per = "{:.3%}".format(Khan_count / len(votes))
@@ -59,10 +93,10 @@ Li_per = "{:.3%}".format(Li_count / len(votes))
 OTooley_per = "{:.3%}".format(OTooley_count / len(votes))
 
 
-print(f'Khan: {Khan_per} ({Khan_count})')
-print(f'Correy: {Correy_per} ({Correy_count})')
-print(f'Li: {Li_per} ({Li_count})')
-print(f"O'Tooley: {OTooley_per} ({OTooley_count})")
+print(f'{candidates[0]}: {Khan_per} ({Khan_count})')
+print(f'{candidates[1]}: {Correy_per} ({Correy_count})')
+print(f'{candidates[2]}: {Li_per} ({Li_count})')
+print(f'{candidates[3]}: {OTooley_per} ({OTooley_count})')
 print("--------------------------")
 print(f'Winner: {Winner}')
 print("--------------------------")
