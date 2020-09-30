8fc57123b6cb5f78118c4e4db53d013bb73886f7
bleeclt <70173407+bleeclt@users.noreply.github.com>
2020-09-30 15:03:51 -0400

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,34 @@
 import os
 import csv
+import collections
+from collections import Counter
 
-election_csv = os.path.join("..", "Resources", "election_data.csv")
+election_csv = os.path.join("Resources", "election_data.csv")
 
 with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader)
+
+    # variable to hold votes list
+    votes = []
+
+    # variable to hold candidates list
+    candidates = []
+
+    # loop to capture all votes 
+    for row in csvreader:
+        votes.append(str(row[2]))
+
+print("Election Results")
+print("--------------------------")
+print(f'Total Votes: {len(votes)}')
+print("--------------------------")
+    
+# dictionary to hold the count of each vote
+breakdown = Counter(votes)
+percentage = 
+
+# get the key and values of each dictionary item
+for key, value in breakdown.items():
+        
+    print(f'{key}: ({value})')
